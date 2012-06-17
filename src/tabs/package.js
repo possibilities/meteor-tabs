b521dc149c6d24d454bf39c6cb149478c08bf2a6
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-16 21:30:44 -0400

    cleanup

--- src/tabs/package.js
+++ src/tabs/package.js
@@ -3,11 +3,15 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+
+  // Dependencies
   api.use('templating', 'client');
   api.use('backbone', 'client');
 
+  // Vendored
   api.add_files('vendor/underscore.strings.js', 'client');
 
+  // Core
   api.add_files('tabs.html', 'client');
   api.add_files('router.js', 'client');
   api.add_files('tabs.js', 'client');
